af583dcd2444c2a890c89f354c0032bbec4f4275
Nicholas Konar <nkonar@protonmail.com>
2024-08-12 03:09:53 -0400

    Refactor user entity

--- src/db/entity/user.ts
+++ src/db/entity/user.ts
@@ -9,66 +9,46 @@ import {
 import { IsEmail, Length } from 'class-validator'
 import argon2 from 'argon2'
 import { assert } from '@util'
-import { EmailAlreadyInUseError, InvalidEmailError } from '@errors/http-errors'
+import { UsernameTakenError } from '@errors/http-errors'
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
-  id!: string
+  id: string
 
   @Length(3, 20)
-  @Column({ unique: true, nullable: true })
-  username?: string
+  @Column({ unique: true })
+  username: string
 
   @Length(8, 100)
-  @Column({ nullable: true })
-  password!: string
+  @Column()
+  password: string
 
   @IsEmail()
   @Column()
-  email!: string
+  email: string
 
   @Column({ default: false })
   emailVerified!: boolean
 
   @CreateDateColumn()
-  createdAt?: Date
+  createdAt: Date
 
   @UpdateDateColumn()
-  updatedAt!: Date
+  updatedAt: Date
 
   constructor(data?: Partial<User>) {
     super()
     Object.assign(this, data)
   }
 
-  public async setLoginCredentials(
-    username: string,
-    password: string
-  ): Promise<this> {
-    const hasCredentials = Boolean(this.username && this.password)
-    if (hasCredentials) {
-      throw new Error('Credentials already set.')
-    }
-    // todo: move config to dedicated module
-    const hash = await argon2.hash(password, {
-      type: 2, // argon2id
-      memoryCost: 2 ** 16, // 64 MiB
-      timeCost: 3, // 3 iterations
-      parallelism: 1, // 1 thread
-    })
-    return this.update({ username, password: hash })
-  }
-
-  public async setUsername(username: string): Promise<this> {
+  public async setUsername(username: string) {
     const usernameTaken = await User.findOneBy({ username })
-    if (usernameTaken) {
-      throw new Error('Username Taken.')
-    }
-    return this.update({ username })
+    assert(!usernameTaken, UsernameTakenError)
+    this.username = username
   }
 
-  public async setPassword(password: string): Promise<this> {
+  public async setPassword(password: string) {
     // todo: move config to dedicated module
     const hash = await argon2.hash(password, {
       type: 2, // argon2id
@@ -76,9 +56,7 @@ export class User extends BaseEntity {
       timeCost: 3, // 3 iterations
       parallelism: 1, // 1 thread
     })
-    return this.update({
-      password: hash,
-    })
+    this.password = hash
   }
 
   public async verifyPassword(password: string): Promise<boolean> {
