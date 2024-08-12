a80039ae3c6f757bf1a7bd0d2980b51566a6215b
Nicholas Konar <nkonar@protonmail.com>
2024-08-12 13:15:15 -0400

    Refactor user entity

--- __tests__/entities/user.test.ts
+++ __tests__/entities/user.test.ts
@@ -1,33 +1,18 @@
-import { AppDataSource } from '@db/data-source'
-import { Repository } from 'typeorm'
 import { User } from '@entity/user'
 
-let repo: Repository<User>
-beforeAll(async () => {
-  repo = AppDataSource.getRepository(User)
-})
-
-afterEach(async () => {
-  await repo.clear()
+beforeEach(async () => {
+  await User.clear()
 })
 
 it('create user', async () => {
-  const email = `foo@bar.com`
-  const user = new User({ email })
-  await user.save()
-  await user.reload()
-  expect(user.id).toBeTruthy()
-  expect(user.email).toBe(email)
-})
-
-it('set username and password', async () => {
   const email = 'foo@bar.com'
   const username = 'foo'
   const password = 'bar'
-  const user = new User({ email })
-  await user.setUsername(username)
+  const user = new User()
+  await user.setEmailOrFail(email)
+  await user.setUsernameOrFail(username)
   await user.setPassword(password)
-  await user.reload()
+  await user.save()
   const valid = await user.verifyPassword(password)
   expect(user.username).toBe(username)
   expect(user.password).toBeTruthy()
