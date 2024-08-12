10041b047c856e057fdf6c700058482faeb24712
Nicholas Konar <nkonar@protonmail.com>
2024-08-12 00:11:15 -0400

    optimize fake user

--- __tests__/__setup__/fakes/user.ts
+++ __tests__/__setup__/fakes/user.ts
@@ -1,5 +1,11 @@
+import crypto from 'crypto'
 import { User } from '@entity/user'
 
 export const fakeUser = async (opts: Partial<User> = {}): Promise<User> => {
-  return new User(opts).save()
+  const user = new User(opts)
+  const random = crypto.randomBytes(4).toString('hex')
+  user.email ||= `${random}@example.com`
+  user.username ||= 'test_user'
+  await user.setPassword(opts.password || 'password')
+  return user.save()
 }
