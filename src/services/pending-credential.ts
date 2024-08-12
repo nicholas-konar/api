0571d46960b56183057c702dfba4e1e4369fa044
Nicholas Konar <nkonar@protonmail.com>
2024-08-12 01:52:26 -0400

    Pending credential service

---
+++ src/services/pending-credential.ts
@@ -0,0 +1,20 @@
+import log from '@logger'
+import crypto from 'crypto'
+import { PendingCredential } from '@entity/pending-credential'
+import { CredentialType } from '@types'
+
+export async function savePendingCredential(
+  credential: string,
+  type: CredentialType
+) {
+  const random = crypto.randomBytes(4).toString('hex')
+  const expiry = parseFloat(process.env.EXPIRY_TIME)
+  const pending = new PendingCredential({
+    token: random,
+    credential,
+    type,
+    expiry,
+  })
+  await pending.save()
+  log.info({ msg: 'Pending credential saved', credentialId: pending.id })
+}
