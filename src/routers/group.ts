7f23d68ee49a531b3f7774db4a9b454ea3496d39
Nicholas Konar <nkonar@protonmail.com>
2024-08-22 13:23:30 -0400

    Use ctx state middleware

--- src/routers/group.ts
+++ src/routers/group.ts
@@ -1,9 +1,10 @@
 import Router from '@koa/router'
 import ctrl from '@controllers/group'
+import { setCtxState } from '@middleware/enrich-context'
 
 const router = new Router({ prefix: '/group' })
 
-router.post('/create', ctrl.create)
-router.post('/update', ctrl.update)
+router.post('/create', setCtxState(['user']), ctrl.create)
+router.post('/update', setCtxState(['user', 'group']), ctrl.update)
 
 export default router
